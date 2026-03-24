a05ca57bd0d8e0e4f9ff7e34630a81f7a65eddc7
yuzu-juice <yuzujuicesec@gmail.com>
2026-03-24 17:12:52 +0900

    update: png size

--- src/components/HelpTagCard.tsx
+++ src/components/HelpTagCard.tsx
@@ -11,8 +11,8 @@ export function HelpTagCard({ iconSrc, text }: HelpTagCardProps) {
       className="flex flex-col overflow-hidden rounded-[2mm] border border-black bg-white"
       style={{ width: `${CARD_WIDTH_MM}mm`, height: `${CARD_HEIGHT_MM}mm` }}
     >
-      <div className="flex flex-1 items-center justify-center bg-white p-[3mm]">
-        <img src={iconSrc} alt="" className="h-[24mm] w-[24mm] object-contain" />
+      <div className="flex flex-1 items-center justify-center bg-white px-[3mm] pb-[3mm] pt-[5mm]">
+        <img src={iconSrc} alt="" className="h-[30mm] w-[30mm] object-contain" />
       </div>
       <div className="flex flex-1 items-center justify-center bg-white p-[2mm]">
         <p
